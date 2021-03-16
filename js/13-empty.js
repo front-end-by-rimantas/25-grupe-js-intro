7ec77f82693e1e6787653e5e1a81b488434411aa
Rimantas <belauzas@yahoo.com>
2021-03-16 13:17:17 +0200

    Sandauga

--- js/13-empty.js
+++ js/13-empty.js
@@ -1,5 +1,5 @@
 function tusciaFunkcija() {
-    return undefined;
+    return false;
 }
 
 console.log(tusciaFunkcija());
---
+++ js/14-multiply.js
@@ -0,0 +1,13 @@
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(44, 5));
+console.log(daugyba(-44, -55));
+console.log(daugyba(-44, 55));
+console.log(daugyba(-44, 0));
+console.log(daugyba(0, 0));
+console.log(daugyba(0, 54850));
+console.log(daugyba(3.14, 2));
